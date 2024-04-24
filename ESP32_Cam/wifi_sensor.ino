3e0d1fe152aa7a8e6988e74548d9023daea0415f
BASIRIN <69839040+REEN-BASIREEN@users.noreply.github.com>
2024-04-24 15:40:09 +0700

    Add files via upload

---
+++ ESP32_Cam/wifi_sensor.ino
@@ -0,0 +1,96 @@
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include "DHT.h"
+
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-4-channel-relay-module
+ */
+#define DHTPIN 26
+#define DHTTYPE DHT22 
+
+#define PIN_RELAY_1  13 // the Arduino pin, which connects to the IN1 pin of relay module
+#define PIN_RELAY_2  12 // the Arduino pin, which connects to the IN2 pin of relay module
+#define PIN_RELAY_3  14 // the Arduino pin, which connects to the IN3 pin of relay module
+#define PIN_RELAY_4  27 // the Arduino pin, which connects to the IN4 pin of relay module
+
+DHT dht(DHTPIN, DHTTYPE);
+// the setup function runs once when you press reset or power the board
+
+void setup() {
+ 
+ // Serial.begin(115200);
+ WifiSetCon();
+ 
+  // initialize digital pin as an output.
+  pinMode(PIN_RELAY_1, OUTPUT);
+  pinMode(PIN_RELAY_2, OUTPUT);
+  pinMode(PIN_RELAY_3, OUTPUT);
+  pinMode(PIN_RELAY_4, OUTPUT);
+  dht.begin();
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+ Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+   Serial.println(F("°C "));
+  Serial.println("Turn on all");
+  digitalWrite(PIN_RELAY_1, HIGH);
+  digitalWrite(PIN_RELAY_2, HIGH);
+  digitalWrite(PIN_RELAY_3, HIGH);
+  digitalWrite(PIN_RELAY_4, HIGH);
+  delay(3000);
+
+  Serial.println("Turn off all");
+  digitalWrite(PIN_RELAY_1, LOW);
+  digitalWrite(PIN_RELAY_2, LOW);
+  digitalWrite(PIN_RELAY_3, LOW);
+  digitalWrite(PIN_RELAY_4, LOW);
+  delay(3000);
+}
+
+
+
+
+void WifiSetCon() {
+    // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+    // it is a good practice to make sure your code sets wifi mode how you want it.
+
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    // wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+     res = wm.autoConnect("Mulberry Ai Reley"); // anonymous ap
+   // res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+    }
+
+}
